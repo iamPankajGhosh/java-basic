cf4350c6a3dff78e4e08c165d1f70beec28ae312
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-01-05 18:09:10 +0530

    appwrite auth service done

---
+++ frontend_react/src/appwrite/auth.js
@@ -0,0 +1,67 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      const userAccount = await this.account.get();
+      if (userAccount) {
+        return userAccount;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
