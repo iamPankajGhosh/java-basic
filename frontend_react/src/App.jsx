9472e6409de93817724aff04565fa13e6fbe4405
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-01-07 14:57:23 +0530

    redux provider config done

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,5 +1,26 @@
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+
 function App() {
-  return <div>App</div>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(setLoading(false));
+  }, []);
+
+  return !loading ? <div>Prime Video Clone</div> : <div>Loading...</div>;
 }
 
 export default App;
